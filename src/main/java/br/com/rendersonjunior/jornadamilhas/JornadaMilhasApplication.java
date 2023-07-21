51dd659e9184d4d4597ac09be5701a1b4751649d
Renderson Junior <rendersonjuniorcmp@gmail.com>
2023-07-21 00:03:53 -0300

    Primeiro Commit no repositorio

---
+++ src/main/java/br/com/rendersonjunior/jornadamilhas/JornadaMilhasApplication.java
@@ -0,0 +1,13 @@
+package br.com.rendersonjunior.jornadamilhas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JornadaMilhasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JornadaMilhasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/rendersonjunior/jornadamilhas/JornadaMilhasApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.rendersonjunior.jornadamilhas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JornadaMilhasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
